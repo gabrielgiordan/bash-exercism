4ca99cb94575ffafec0e337f5f24cc4ac60d7f81
gabrielgiordan <hello@gabrielgiordano.com>
2021-08-16 19:27:32 -0300

    Add comments to reverse_string.sh

--- reverse-string/reverse_string.sh
+++ reverse-string/reverse_string.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-for (( i=${#1}; i-- > 0; )) do 
+# `(())` double parenthesis allow to execute arithmetic operations.
+# `${1}` gets the 1st argument.
+# `${#variable}` gets the variable's length.
+# `${variable:position:length}` gets a substring.
+# `echo -n` prints omitting a new line.
+
+for (( i=${#1}; i-- > 0; )) do
     echo -n "${1:$i:1}"
 done
