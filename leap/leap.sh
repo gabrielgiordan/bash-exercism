b6802723ccfbf8691cb51cc47a96699051ce17e7
gabrielgiordan <hello@gabrielgiordano.com>
2021-08-16 19:27:41 -0300

    Add comments to leap.sh

--- leap/leap.sh
+++ leap/leap.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# `$#` get the number of arguments.
+# `-eq` numeric equal comparator.
+# `=~` regex match.
+# `[[]]` test execution.
+# `(())` arithmetic execution.
+# `%` mod operator.
+# `==` used instead of `-eq` when onto an arithmetic context.
+
 ! [[ $# -eq 1 && "$1" =~ ^[0-9]+$ ]] && \
 echo "Usage: leap.sh <year>" && exit 1;
 
